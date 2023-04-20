98c6f3810c22b4f66e90fff2a1acf8f770b5c6e6
Emiliya Melnikova <binarytoucan@gmail.com>
2023-04-20 15:43:24 +0300

    Changed BasePage and Cleaned MainPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from .locators import MainPageLocators
 
 MAIN_URL = "http://selenium1py.pythonanywhere.com/"
 
@@ -8,11 +7,3 @@ class MainPage(BasePage):
 
     def __init__(self, browser):
         super().__init__(browser, MAIN_URL)
-    
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-        
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
